16f8e823c4348b1f1f9dd3eb9667cdc559b25523
notebyte7 <notebyte@yandex.ru>
2023-10-12 09:24:57 +0300

    feat: start

---
+++ src/main/java/com/notebyte7/measurementparser/MeasurementParserApplication.java
@@ -0,0 +1,13 @@
+package com.notebyte7.measurementparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notebyte7/measurementparser/MeasurementParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notebyte7.measurementparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
